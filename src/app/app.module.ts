f58a5f9bc129e484930cb7f85ac3deb31d875b29
Bart Vets <bart_vets@hotmail.com>
2018-06-13 00:02:14 +0200

    cleanup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,35 +6,9 @@ import { NgDatatableModule } from './modules/ngDatatable/ngdatatable.module';
 
 import { AppComponent } from './app.component';
 
-// import { NgDatatableComponent } from './modules/ngDatatable/ngdatatable.component.ng2-datatable';
-// import { 
-//     NgDatatableColumnComponent, 
-//     NgDatatableColumnBodyTemplateLoader, 
-//     NgDatatableColumnHeaderTemplateLoader, 
-//     NgDatatableColumnExpandableBodyTemplateLoader, 
-//     NgDatatableColumnExpandableHeaderTemplateLoader } from './modules/ngDatatable/ngdatatable.component.column';
-// import { NgDatatableNumberColumnComponent } from './modules/ngDatatable/ngdatatable.component.number-column';
-// import { NgDatatableBooleanColumnComponent } from './modules/ngDatatable/ngdatatable.component.boolean-column';
-// import { NgDatatableDateColumnComponent } from './modules/ngDatatable/ngdatatable.component.date-column';
-// import { NgDatatableExpandableRowComponent } from './modules/ngDatatable/ngdatatable.component.expandable-row';
-// import { NgDatatablePaginationComponent } from './modules/ngDatatable/ngdatatable.component.pagination';
-// import { NgDatatableActionOnTableComponent } from './modules/ngDatatable/ngdatatable.component.action-on-table';
-
 @NgModule({
   declarations: [
     AppComponent
-    // NgDatatableComponent,
-    // NgDatatableColumnComponent,
-    // NgDatatableColumnBodyTemplateLoader,
-    // NgDatatableColumnHeaderTemplateLoader, 
-    // NgDatatableColumnExpandableBodyTemplateLoader, 
-    // NgDatatableColumnExpandableHeaderTemplateLoader,
-    // NgDatatableNumberColumnComponent,
-    // NgDatatableBooleanColumnComponent,
-    // NgDatatableDateColumnComponent,
-    // NgDatatableExpandableRowComponent,
-    // NgDatatablePaginationComponent,
-    // NgDatatableActionOnTableComponent
   ],
   imports: [
     BrowserModule,
