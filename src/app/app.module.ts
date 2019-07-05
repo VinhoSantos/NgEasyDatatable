a1773e797a5c21d5a5d89c6788bfbbd9c1856844
Bart Vets <bart.vets@qframe.be>
2019-07-05 22:26:43 +0200

    updated @angular/core 6.1.10 -> 8.1.0

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { HttpClientModule } from '@angular/http';
 import { NgEasyDatatableModule } from './modules/ngEasyDatatable/ng-easy-datatable.module';
 
 import { AppComponent } from './app.component';
@@ -13,7 +13,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule,
+    HttpClientModule,
     NgEasyDatatableModule
   ],
   providers: [],
